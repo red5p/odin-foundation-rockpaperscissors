9b943002302c9e456773e31cadac48df488379c7
Ryo <wh404@outlook.com>
2021-09-29 00:00:49 +0100

    Modified UI and added some new logic to js

--- js/logics.js
+++ js/logics.js
@@ -18,8 +18,26 @@ function computerPlay() {
 }
 
 function playRound(e) {
-    const playerSelection = e.target.id;
+    const playerSelection = this.id;
     const computerSelection = computerPlay();
+
+    const playerIcon = document.querySelector('#playerAnimation');
+    if (playerSelection === 'rock') {
+        playerIcon.innerHTML = '<i class="far fa-hand-rock fa-5x"></i>';
+    } else if (playerSelection === 'paper') {
+        playerIcon.innerHTML = '<i class="far fa-hand-paper fa-5x"></i>';
+    } else {
+        playerIcon.innerHTML = '<i class="far fa-hand-scissors fa-5x"></i>';
+    }
+    const computerIcon = document.querySelector('#computerAnimation');
+    if (computerSelection === 'rock') {
+        computerIcon.innerHTML = '<i class="far fa-hand-rock fa-5x"></i>';
+    } else if (computerSelection === 'paper') {
+        computerIcon.innerHTML = '<i class="far fa-hand-paper fa-5x"></i>';
+    } else {
+        computerIcon.innerHTML = '<i class="far fa-hand-scissors fa-5x"></i>';
+    }
+    
 
     const playerScore = document.querySelector('#playerScore');
     const computerScore = document.querySelector('#computerScore');
@@ -33,12 +51,20 @@ function playRound(e) {
             playerSelection === 'scissors' && computerSelection === 'paper'
     ) {
         score = Number(playerScore.textContent) + 1;
+        playerIcon.style.color = 'rgb(255, 196, 0)';
+        computerIcon.style.color = 'black';
+        document.querySelector('#scoreLeft').style.color = 'rgb(255, 196, 0)';
+        document.querySelector('#scoreRight').style.color = 'black';
         playerScore.textContent = score;
         if (score === 5) {
             announceWinner(0);
         }
     } else {
         score = Number(computerScore.textContent) + 1;
+        playerIcon.style.color = 'black';
+        computerIcon.style.color = 'rgb(255, 196, 0)';
+        document.querySelector('#scoreLeft').style.color = 'black';
+        document.querySelector('#scoreRight').style.color = 'rgb(255, 196, 0)';
         computerScore.textContent = score;
         if (score === 5) {
             announceWinner(1);
@@ -48,9 +74,13 @@ function playRound(e) {
 
 function announceWinner(winner) {
     const announcement = document.querySelector('#announcement');
-    announcement.textContent = (winner === 0) ?
-                               'You WIN! Congratulations!' :
-                               'You lose. Wanna play again?';
+    if (winner === 0) {
+        announcement.textContent = 'You WIN! Congratulations!';
+        announcement.style.color = 'rgb(255, 196, 0)';
+    } else {
+        announcement.textContent = 'You lose. Wanna play again?';
+        announcement.style.color = 'black';
+    }
     announcement.style.visibility = 'visible';
 
     buttons.forEach((button) => {
@@ -76,5 +106,5 @@ buttons.forEach((button) => {
     button.addEventListener('click', playRound);
 });
 
-const playAgainButton = document.querySelector('.playAgainButton');
+const playAgainButton = document.querySelector('#playAgainButton');
 playAgainButton.addEventListener('click', playAgain);
